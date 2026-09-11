68fe1a7ce8030abbecf56e33d89a1738df52e811
TOMAKAIROS <yatroyoa@gmail.com>
2026-09-11 18:21:05 -0500

    Separate bot name from display formatting ' : '

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,23 @@
 print("Hello")
 
-botName = "Jarvis: "
+botName = "Jarvis"
+bot = botName + ": "
 
 while True:
 
-    why = input(botName + "Why are you building this?")
+    why = input(bot + "Why are you building this?")
 
     why = why.strip().lower()
 
     if why == "quit":
-        print(botName + "Goodbye...")
+        print(bot + "Goodbye...")
         break
     
     if why == "to learn" or why == "learn":
-        print(botName + "You're on the right path.")
+        print(bot + "You're on the right path.")
     elif why == "idk":
-        print(botName + "Go figure it out!")
+        print(bot + "Go figure it out!")
     elif why == "":
         print("say something...i'm giving up on you.\n")
     else:
-        print(botName + "awkward...")
+        print(bot + "awkward...")
