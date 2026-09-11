2b8b3e665dba3e1ff9bf6b7983cb564f91be5023
TOMAKAIROS <yatroyoa@gmail.com>
2026-09-11 17:45:41 -0500

    Add chatbot loop, conditional responses, quit command, and input cleanup

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,22 @@
-TOMAKAIROS = input("To obtain every opportunity.")
+print("Hello")
 
-print(TOMAKAIROS)
+botName = "Jarvis: "
+
+while True:
+
+    why = input(botName + "Why are you building this?")
+
+    why = why.strip().lower()
+
+    if why == "quit":
+        print(botName + "Goodbye...")
+        break
+    
+    if why == "to learn" or why == "learn":
+        print(botName + "You're on the right path.")
+    elif why == "idk":
+        print(botName + "Go figure it out!")
+    elif why == "":
+        print("say something...i'm giving up on you.\n")
+    else:
+        print(botName + "awkward...")
