e2da755d9daa4ed1eef9c8eedbdc7986b739c47a
TOMAKAIROS <yatroyoa@gmail.com>
2026-09-12 19:48:53 -0500

    Added History and Count variable to be used in the maintenance question function

--- task1.py
+++ task1.py
@@ -1,2 +1,12 @@
 print("Welcome to my text based program that records maintenance done on my Subaru.")
 
+history = []
+count = 0
+
+def maintenance_question(myAnswer)
+    if myAnswer == "history":
+        return history
+    elif myAnswer == "count":
+        return count
+    elif myAnswer == "quit":
+        return "Goodbye..."
